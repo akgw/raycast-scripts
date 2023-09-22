75336f3fba65a749f2adb53b7c0bf0c1a81799c4
akgw <r.akagawa@buysell-technologies.com>
2023-09-22 12:06:32 +0900

    modify use cachedState

--- extensions/hatebu-reader/src/index.tsx
+++ extensions/hatebu-reader/src/index.tsx
@@ -1,6 +1,7 @@
 import { Icon, MenuBarExtra, open } from "@raycast/api";
-import {parse} from "node-html-parser"
-import { useCallback, useEffect, useState } from "react";
+import { useCachedState } from "@raycast/utils";
+import { parse } from "node-html-parser";
+import { useCallback, useEffect } from "react";
 import RssParser from "rss-parser";
 
 const parser = new RssParser();
@@ -14,9 +15,9 @@ type Feed = {
 };
 
 export default function Command() {
-  const [feedList, setFeedList] = useState<Feed[]>([]);
-  const fetchData = useCallback(async() => {    
-    const response = await parser.parseURL("https://b.hatena.ne.jp/hotentry/it.rss")
+  const [feedList, setFeedList] = useCachedState<Feed[]>("");
+  const fetchData = useCallback(async () => {
+    const response = await parser.parseURL("https://b.hatena.ne.jp/hotentry/it.rss");
     const parsed = response.items.map((item) => {
       return {
         date: item.date,
@@ -29,33 +30,34 @@ export default function Command() {
             ?.getAttribute("src")
             ?.toString() || Icon.Checkmark,
       };
-    })
+    });
 
-    setFeedList(parsed)
-  }, [])
+    setFeedList(parsed);
+  }, [setFeedList]);
 
   useEffect(() => {
     void fetchData();
-  }, [fetchData])
+  }, [fetchData]);
 
   const stringSlice = (str: string) => {
-    return str.slice(0, 100) + (str.length > 100 ? "..." : "")
-  }
+    return str.slice(0, 100) + (str.length > 100 ? "..." : "");
+  };
 
   return (
     <MenuBarExtra title="はてブ" icon="https://b.hatena.ne.jp/favicon.ico">
       <MenuBarExtra.Section title="テクノロジー" />
-      {feedList.map(feed => {
-        return (
-          <MenuBarExtra.Item
-            key={feed.link}
-            icon={feed.imageUrl} 
-            title={stringSlice(feed.title)} 
-            subtitle={stringSlice(feed.description)}
-            onAction={() => open(feed.link)}
-          />
-        )
-      })}
+      {feedList &&
+        feedList.map((feed) => {
+          return (
+            <MenuBarExtra.Item
+              key={feed.link}
+              icon={feed.imageUrl}
+              title={stringSlice(feed.title)}
+              subtitle={stringSlice(feed.description)}
+              onAction={() => open(feed.link)}
+            />
+          );
+        })}
     </MenuBarExtra>
   );
 }
